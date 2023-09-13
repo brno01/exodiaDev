f83e55cae4118048d1bb128ee3b028937278a08d
Bruno Oliveira <contato.bruno0dev@gmail.com>
2023-09-13 18:10:42 -0300

    confirm

--- exodia.py
+++ exodia.py
@@ -1,41 +1,28 @@
 import os
-import re
-
-# Função para obter o nome a partir do arquivo .cnf
-def get_name_from_cnf(file_path):
-    with open(file_path, 'r') as cnf_file:
-        # Lê a primeira linha do arquivo .cnf
-        first_line = cnf_file.readline()
-        # Use uma expressão regular para encontrar o nome
-        match = re.search(r'Nome:\s*(\w+)', first_line)
-        if match:
-            return match.group(1)
-    return None
 
 # Diretório raiz onde estão as pastas com os arquivos .cnf
-root_directory = "/caminho/do/seu/diretorio/raiz"
+root_directory = "test/"
 
 # Percorre todas as pastas no diretório raiz
 for folder_name in os.listdir(root_directory):
     folder_path = os.path.join(root_directory, folder_name)
-    
+
     # Verifica se é uma pasta
     if os.path.isdir(folder_path):
         # Encontra arquivos .cnf dentro da pasta
         cnf_files = [f for f in os.listdir(folder_path) if f.endswith('.cnf')]
-        
+
         # Verifica se há pelo menos um arquivo .cnf na pasta
         if cnf_files:
             # Assume que estamos usando o primeiro arquivo .cnf encontrado
             cnf_file_path = os.path.join(folder_path, cnf_files[0])
-            new_name = get_name_from_cnf(cnf_file_path)
-            
-            # Se um nome foi encontrado no arquivo .cnf, renomeia a pasta
-            if new_name:
-                new_folder_path = os.path.join(root_directory, new_name)
-                os.rename(folder_path, new_folder_path)
-                print(f"A pasta {folder_name} foi renomeada para {new_name}")
-            else:
-                print(f"Não foi possível encontrar um nome válido no arquivo .cnf da pasta {folder_name}")
+
+            # Lê a primeira linha do arquivo .cnf
+            with open(cnf_file_path, 'r') as cnf_file:
+                first_line = cnf_file.readline().strip()  # Remove espaços em branco
+                # Renomeia a pasta com o conteúdo da primeira linha do arquivo .cnf
+                os.rename(folder_path, os.path.join(
+                    root_directory, first_line))
+                print(f"A pasta {folder_name} foi renomeada para {first_line}")
         else:
             print(f"Nenhum arquivo .cnf encontrado na pasta {folder_name}")
